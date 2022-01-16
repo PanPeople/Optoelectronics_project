8f1f313e1d376b9cdfcd4f75b9342b6a2d72dd0f
PanPeople <mikolajek26@gmail.com>
2022-01-16 17:11:33 +0100

    updated python script

--- Opto_dev_control.py
+++ Opto_dev_control.py
@@ -1,13 +1,32 @@
 import serial
 import time
 
-arduino = serial.Serial(port = "COM4", baudrate = 115200)
+arduino = serial.Serial(port = "COM5", baudrate = 115200)
 
-for i in range(10):
-    arduino.write('en1'.encode())
-    print("ON")
-    time.sleep(1)
+string = ""
 
-    arduino.write('en0'.encode())
-    print("OFF")
-    time.sleep(1)
+
+arduino.write("010".encode())
+print("010")
+time.sleep(1)
+
+arduino.write('en1'.encode())
+print("ON")
+time.sleep(1)
+
+
+
+for i in range(9):
+
+    if (i > 1):
+        string = str(i*10)
+        
+        string = "0" + string
+        
+        arduino.write(string.encode())
+        print(string)
+        time.sleep(1)
+
+arduino.write('en0'.encode())
+print("OFF")
+time.sleep(1)
